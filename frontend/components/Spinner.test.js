fbd6cd44f222ace6e66dd1c89e64def5f8ece276
Sawyer <sawyer.welter@gmail.com>
2023-03-01 19:12:58 -0600

    Spinner testing added in Spinner.test.js

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,16 @@
+import React from "react"
+
+import Spinner from "./Spinner.js";
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(true)
+test('spinner on', () => {
+  render(< Spinner on={true}/>)
+  expect(screen.getByText(/Please wait.../i)).toBeInTheDocument();
+})
+
+test('spinner off', () => {
+  render(< Spinner on={false}/>)
+  expect(screen.queryByText(/Please wait.../i)).not.toBeInTheDocument();
 })
